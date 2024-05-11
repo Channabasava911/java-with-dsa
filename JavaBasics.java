462887c159108dbe3a020445bbd86aff0745b042
Channabasava911 <153277571+Channabasava911@users.noreply.github.com>
2024-05-11 23:55:09 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,15 @@
+
+public class JavaBasics
+{
+public static void main(String args[])
+{
+System.out.println("Hi java");
+System.out.print("Hi java\n");
+}
+}
+
+
+
+
+
+
---
+++ Jinput.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Jinput
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+/*String in=sc.next();
+System.out.println(in);*/
+String name=sc.nextLine();
+System.out.println(name);
+int no=sc.nextInt();
+System.out.println(no);
+float price=sc.nextFloat();
+System.out.println(price);
+}
+}
