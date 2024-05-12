69cb271fdf58a70b78d6bbd67464b7074f2e5fe7
Channabasava911 <153277571+Channabasava911@users.noreply.github.com>
2024-05-12 23:38:00 +0530

    Add files via upload

---
+++ Typeconv.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Typeconv
+{
+public static void main(String args[])
+{
+/*
+int a=25;
+long b=a;
+System.out.println(b);*/
+
+/*
+Scanner sc=new Scanner(System.in);
+float no=sc.nextInt();
+System.out.println(no);*/
+
+//Type casting
+/*
+float a=25.9999999999f;
+int b=(int)a;
+System.out.println(b);
+
+char ch='A';
+int no=ch;
+System.out.println(no);*/
+
+//type promotion
+
+/*char a='A';
+short b=50;
+int sum=a+b;
+System.out.println(sum);
+
+short a=5;
+byte b=25;
+char c='c';
+byte be=(byte)(a+b*c);
+System.out.println(be);*/
+
+/*
+char a='a';
+char b='b';
+char c=(char)(b-a);
+System.out.println((int)(b));
+System.out.println((int)(a));
+System.out.println(a);
+System.out.println(b-a);
+
+int a=10;
+float b=20.25f;
+long c=25;
+double d=30;
+double ans=a+b+c+d;
+System.out.println(ans);*/
+
+byte b=5;
+b=(byte)(b*2);
+System.out.println(b);
+
+
+
+
+}
+}
