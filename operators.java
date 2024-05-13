182fb6b59c54e0aec1a789726b496ba950c573df
Channabasava911 <153277571+Channabasava911@users.noreply.github.com>
2024-05-13 23:58:51 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,35 @@
+//unary
+
+
+public class operators
+{
+    public static void main(String args[])
+    {
+        //pre-increment
+        int ai=10;
+        int bi=++ai;
+        System.out.println(ai);
+        System.out.println(bi);
+
+        //post-increment
+        int ap=10;
+        int bp=ap++;
+        System.out.println(ap);
+        System.out.println(bp);
+
+        //pre-decrement
+        int ad=10;
+        int bd=--ad;
+        System.out.println(ad);
+        System.out.println(bd);
+
+        //post-decrement
+        int apd=10;
+        int bpd=apd--;
+        System.out.println(apd);
+        System.out.println(bpd);
+
+
+    }
+
+}
