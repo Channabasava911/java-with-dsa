afcf0c916ee645d972155d0be2967b7e29d71f56
Channabasava911 <153277571+Channabasava911@users.noreply.github.com>
2024-05-29 23:44:25 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class reversearray
+{
+    public static void reversearr(int num[])
+    {
+        int first=0;
+        int last=num.length-1;
+        while(first<=last)
+        {
+            int temp=num[last];
+            num[last]=num[first];
+            num[first]=temp;
+            first++;
+            last--;
+
+        }
+    }
+    public static void main(String args[])
+    {
+        int num[]={2,4,6,8,10};
+        reversearr(num);
+        for(int i=0;i<num.length;i++)
+        {
+            System.out.print(num[i]+" ");
+        }
+    
+    }
+}
